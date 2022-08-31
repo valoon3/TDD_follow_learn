349f9b5d18b360b4052fb490575c6f51fb4127de
valoon3 <valoon2@naver.com>
2022-08-31 19:17:19 +0900

    첫번째 테스트 스터디

--- ch1/src/controller/product.js
+++ ch1/src/controller/product.js
@@ -1,3 +1,9 @@
+const productModel = require('../model/Product')
+
 exports.hello = (req, res) => {
     res.send('안녕하세요!!!');
+}
+
+exports.createProduct = (req, res, next) => {
+    productModel.create(req.body);
 }
---
+++ ch1/test/unit/products.test.js
@@ -0,0 +1,40 @@
+const productController = require('../../src/controller/product');
+const productModel = require('../../src/model/Product');
+const httpMocks = require('node-mocks-http');
+const newProduct = require('../data/new-product.json');
+
+// beforeEach : 여러개의 테스트 안에 공통된 Code가 있다면 beforeEach 안에 넣어서 줄여줄 수 있습니다.
+
+productModel.create = jest.fn(); // 목 함수 생성
+
+let req, res, next;
+beforeEach(() => {
+    [req, res] = [httpMocks.createRequest(), httpMocks.createResponse()];
+    next = null;
+})
+
+describe('jest test', () => {
+    test('two plus two is four', () => {
+        expect(2+2).toBe(4);
+    })
+
+    test('two plus two is not five', () => {
+        expect(2+2).not.toBe(5);
+    })
+})
+
+describe('Product Controller Create', () => {
+    beforeEach(() => {
+        req.body = newProduct;
+    })
+
+    it('should have a createProduct function', () => {
+        expect(typeof productController.createProduct).toBe('function')
+    })
+    it('should call Product modelCreate', () => {
+
+
+        productController.createProduct(req, res, next);
+        expect(productModel.create).toBeCalledWith(newProduct);
+    })
+})
