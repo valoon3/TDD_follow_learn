d32666feadf824c296d3e753fefea65423c18dbd
valoon3 <valoon2@naver.com>
2023-03-10 16:40:44 +0900

    refactor test 코드파일 학습을 위한 코드 삭제

--- ch1/test/unit/products.test.js
+++ ch1/test/unit/products.test.js
@@ -13,16 +13,6 @@ beforeEach(() => {
     next = null;
 })
 
-describe('jest test', () => {
-    test('two plus two is four', () => {
-        expect(2+2).toBe(4);
-    })
-
-    test('two plus two is not five', () => {
-        expect(2+2).not.toBe(5);
-    })
-})
-
 describe('Product Controller Create', () => {
     beforeEach(() => {
         req.body = newProduct;
